25b8fd0668d921f2780028a5ee8527a0c514e9b5
farisfian06 <farisfian06@gmail.com>
2025-02-27 22:30:06 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CommentController;
+use App\Models\Comment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +11,9 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/articles', [ArticleController::class, 'index']); 
+Route::get('/articles/{id}', [ArticleController::class, 'detail']); 
+Route::post('/articles', [ArticleController::class, 'add']); 
+Route::put('/articles/{id}', [ArticleController::class, 'update']); 
+Route::delete('/articles/{id}', [ArticleController::class, 'delete']); 
+Route::delete('/comments/{id}', [CommentController::class, 'delete']); 
+Route::post('/articles/{articleId}/comments', [CommentController::class, 'add']);
